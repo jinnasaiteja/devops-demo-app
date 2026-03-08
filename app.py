34fc2b050873356f7694f93eca3e7840d4bd89d5
jinnasaiteja <svas60058@gmail.com>
2026-03-08 11:16:56 -0400

    updated app message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route("/")
 def hello():
-    return"Hello from Devops CI/CD Pipeline"
+    return"Hello from Devops CI/CD Pipeline - Version 2"
 
 if _name_ == "_main_":
     app.run(host="0.0.0.0", port=5000)
